13decc25db796a87638a429a50df5a8c359f5354
Ali Chaayb <achaaybdev@gmail.com>
2021-11-10 13:59:52 +0100

    done

--- C13.c
+++ C13.c
@@ -4,6 +4,13 @@
 
 int main(int argc, char **argv){
     
-    //work in progress
+    int nb;
+    
+    printf("Saisire Nombre : \n");
+    scanf("%d", &nb);
 
+    printf("Hexadecimal : %08x\n", nb);
+    printf("Octale : %011o\n", nb);
+
+    return 0;
 }
